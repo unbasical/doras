e4dd4c6391d4fb86bcac8e5e12aaa1f8fc4c25eb
Victor Embacher <victor.embacher@unbasical.com>
2025-01-16 08:08:05 +0100

    remove unused code and add doc comment

--- internal/pkg/api/apicommon/apicommon.go
+++ internal/pkg/api/apicommon/apicommon.go
@@ -1,34 +1,10 @@
 package apicommon
 
 import (
-	"context"
-	"path"
-
-	v1 "github.com/opencontainers/image-spec/specs-go/v1"
-
-	"oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/registry/remote"
 )
 
+// Config is used to configure repository clients for Server.
 type Config struct {
 	RepoClients map[string]remote.Client
-}
-
-type RegistryStorage struct {
-	baseRepo string
-	reg      *remote.Registry
-}
-
-func (r *RegistryStorage) GetDeltaStorage(from, to v1.Descriptor) (string, oras.Target, error) {
-	repoPath := path.Join(
-		from.Digest.Encoded(),
-		to.Digest.Encoded(),
-	)
-	return r.GetStorage(repoPath)
-}
-
-func (r *RegistryStorage) GetStorage(repoPath string) (string, oras.Target, error) {
-	repoPath = path.Join(r.baseRepo, repoPath)
-	repo, err := r.reg.Repository(context.Background(), repoPath)
-	return repoPath, repo, err
 }
