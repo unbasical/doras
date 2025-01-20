e3139589b02d548a1ab91f94cef31c5a2d3ba98f
Victor Embacher <victor.embacher@unbasical.com>
2025-01-20 11:49:27 +0100

    fix name

--- cmd/doras-cli/pull.go
+++ cmd/doras-cli/pull.go
@@ -1,6 +1,9 @@
 package main
 
+import "context"
+
 // pull image from the registry and use delta updates if possible
-func (*cliArgs) pull() error {
+func (*cliArgs) pull(ctx context.Context) error {
+	_ = ctx
 	panic("not implemented")
 }
