593fc13c75c24d4448767510f38e2de3f32bb277
Victor Embacher <victor.embacher@unbasical.com>
2025-01-27 09:27:15 +0100

    implement loading the credentials from the config file

---
+++ internal/pkg/utils/ociutils/credentials.go
@@ -0,0 +1,37 @@
+package ociutils
+
+import (
+	"context"
+	"errors"
+	"github.com/sirupsen/logrus"
+	"oras.land/oras-go/v2/registry/remote/auth"
+)
+
+type CredFuncAggregate struct {
+	credFuncs []auth.CredentialFunc
+}
+
+// NewCredentialsAggregate aggregates multiple auth.CredentialFunc into one function.
+// Loads in the order they were added and returns an error if none are found.
+func NewCredentialsAggregate(opts ...func(aggregate *CredFuncAggregate)) auth.CredentialFunc {
+	aggregate := &CredFuncAggregate{}
+	for _, opt := range opts {
+		opt(aggregate)
+	}
+	return func(ctx context.Context, hostport string) (auth.Credential, error) {
+		for _, credFunc := range aggregate.credFuncs {
+			cred, err := credFunc(ctx, hostport)
+			if err == nil {
+				return cred, nil
+			}
+			logrus.WithError(err).Debugf("failed to load credentials for %v", hostport)
+		}
+		return auth.Credential{}, errors.New("no credential function found")
+	}
+}
+
+func WithCredFunc(cf auth.CredentialFunc) func(*CredFuncAggregate) {
+	return func(c *CredFuncAggregate) {
+		c.credFuncs = append(c.credFuncs, cf)
+	}
+}
