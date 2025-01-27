ed4ad6119c5c5c8db76108947176783d1daf0e02
Victor Embacher <victor.embacher@unbasical.com>
2025-01-27 08:51:15 +0100

    update example config with comments and add way to print it from the server

--- configs/configfile.go
+++ configs/configfile.go
@@ -13,8 +13,11 @@ type CLI struct {
 	ConfigFilePath       string `help:"Path to the Doras server config file." env:"DORAS_CONFIG_FILE_PATH"`
 	DockerConfigFilePath string `help:"Path to the docker config file which is used to access registry credentials." default:"~/.docker/config.json" env:"DOCKER_CONFIG_FILE_PATH"`
 	LogLevel             string `help:"Server log level." default:"info" enum:"debug,info,warn,error" env:"DORAS_LOG_LEVEL"`
-	ShutdownTimout       uint   `help:"Graceful shutdown timeout (in seconds)." default:"20" env:"DORAS_SHUTDOWN_TIMEOUT"`
+	ShutdownTimout       uint   `help:"Graceful shutdowxn timeout (in seconds)." default:"20" env:"DORAS_SHUTDOWN_TIMEOUT"`
 	InsecureAllowHTTP    bool   `help:"Allow INSECURE HTTP connections." default:"false" env:"DORAS_INSECURE_ALLOW_HTTP"`
+	ExampleConfig        struct {
+		Output string `help:"Write example config to this location instead of printing to stdout." type:"path"`
+	} `cmd:"" help:"Print or store example config."`
 }
 
 // ServerConfigFile is used to parse the config files that can be used for more extensive configuration.
