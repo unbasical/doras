48584f3c8c1cb34a2585ef3a83e8783f86c60d3a
Victor Embacher <victor.embacher@unbasical.com>
2025-01-30 09:07:47 +0100

    add p-tests to differs

--- internal/pkg/delta/bsdiff/creator_test.go
+++ internal/pkg/delta/bsdiff/creator_test.go
@@ -1,7 +1,11 @@
 package bsdiff
 
 import (
+	"bytes"
+	bsdiff2 "github.com/gabstv/go-bsdiff/pkg/bsdiff"
 	"github.com/unbasical/doras/pkg/algorithm/delta"
+	"io"
+	"strings"
 	"testing"
 )
 
@@ -11,4 +15,26 @@ func TestDiffer_Interface(t *testing.T) {
 	if !ok {
 		t.Error("interface not implemented")
 	}
+}
+
+func TestCreator_Diff(t *testing.T) {
+	differ := NewCreator()
+	from := "hello"
+	to := "world"
+	expected, err := bsdiff2.Bytes([]byte(from), []byte(to))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rc, err := differ.Diff(strings.NewReader(from), strings.NewReader(to))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := io.ReadAll(rc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(got, expected) {
+		t.Errorf("differ.Diff() = %x, want %x", got, expected)
+	}
+	_ = rc.Close()
 }
--- internal/pkg/delta/tardiff/creator_test.go
+++ internal/pkg/delta/tardiff/creator_test.go
@@ -1,7 +1,10 @@
 package tardiff
 
 import (
+	"bytes"
+	"github.com/unbasical/doras/internal/pkg/utils/fileutils"
 	"github.com/unbasical/doras/pkg/algorithm/delta"
+	"io"
 	"testing"
 )
 
@@ -11,4 +14,23 @@ func TestDiffer_Interface(t *testing.T) {
 	if !ok {
 		t.Error("interface not implemented")
 	}
+}
+
+func TestCreator_Diff(t *testing.T) {
+	differ := NewCreator()
+	from := fileutils.ReadOrPanic("../../../../test/test-files/from.tar.gz")
+	to := fileutils.ReadOrPanic("../../../../test/test-files/to.tar.gz")
+	expected := fileutils.ReadOrPanic("../../../../test/test-files/delta.patch.tardiff")
+	rc, err := differ.Diff(bytes.NewReader(from), bytes.NewReader(to))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := io.ReadAll(rc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(got, expected) {
+		t.Errorf("differ.Diff() = %x, want %x", got, expected)
+	}
+	_ = rc.Close()
 }
