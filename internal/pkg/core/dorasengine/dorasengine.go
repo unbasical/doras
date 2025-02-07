767c53a5da010b5fd1f93850be901c2e09c080e2
Victor Embacher <victor.embacher@unbasical.com>
2025-02-07 08:44:33 +0100

    add logging line

--- internal/pkg/core/dorasengine/dorasengine.go
+++ internal/pkg/core/dorasengine/dorasengine.go
@@ -155,6 +155,7 @@ func readDelta(ctx context.Context, registry registrydelegate.RegistryDelegate, 
 		return
 	}
 	if toDescriptor.Digest == fromDescriptor.Digest {
+		log.Debugf("got request for images with identical digests %v, %v", fromDescriptor.Digest, toDescriptor.Digest)
 		apiDelegate.HandleError(error2.ErrIncompatibleArtifacts, "from and to image are identical")
 		return
 	}
