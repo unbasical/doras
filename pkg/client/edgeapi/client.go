a77e169e42779e39de5837ff4f0b6fc048d0e908
Victor Embacher <victor.embacher@unbasical.com>
2025-02-11 08:57:23 +0100

    add more context to error message

--- pkg/client/edgeapi/client.go
+++ pkg/client/edgeapi/client.go
@@ -99,7 +99,7 @@ func (c *deltaApiClient) ReadDeltaAsync(from, to string, acceptedAlgorithms []st
 		decoder.DisallowUnknownFields()
 		err = decoder.Decode(&errBody)
 		if err != nil {
-			return nil, false, fmt.Errorf("unknown error %v, %w", resp.Status, err)
+			return nil, false, fmt.Errorf("unknown error for request to %q StatusCode: %q, %w", url, resp.Status, err)
 		}
 		return nil, false, errBody
 	}
